4ea03bb1c507b92a460f90d5f551291fd082bac8
stephdanid <stephaniedanidominguez@gmail.com>
2020-12-11 14:21:57 -0600

    created OauthServer.js, created async function to postData, api is currently able to serach for gaming character, bodyparamvariable created, working on getting artwork from another end point,BE SURE TO COMMENT BACK CONST DOTENV CONST RESPONSE AND MODULE EXPORTS. ESLINT CHECK DIDNT LIKE THEM

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const db = require('./models');
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 // We need to use sessions to keep track of our user's login status
 app.use(
-    session({secret: 'keyboard cat', resave: true, saveUninitialized: true}),
+    session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }),
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -24,15 +24,16 @@ app.use(passport.session());
 // Requiring our routes
 require('./routes/html-routes.js')(app);
 require('./routes/api-routes.js')(app);
+require('./routes/oAuthServer.js')(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-        '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
-        PORT,
-        PORT,
-        console.log(process.env),
-    );
-  });
+    app.listen(PORT, () => {
+        console.log(
+            '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+            PORT,
+            PORT,
+            console.log(process.env),
+        );
+    });
 });
