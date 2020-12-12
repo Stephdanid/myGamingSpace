cb28009db69140fa6e0e63dbd4f6c87a9a77a072
stephdanid <stephaniedanidominguez@gmail.com>
2020-12-12 00:52:44 -0600

    created gamesearch function, that is asynchronous, bodyparam for search created, uncomment out the top 2 vairables in oauthserver.js to run code, eslint didn't like the being declared

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const db = require('./models');
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 // We need to use sessions to keep track of our user's login status
 app.use(
-    session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }),
+    session({secret: 'keyboard cat', resave: true, saveUninitialized: true}),
 );
 app.use(passport.initialize());
 app.use(passport.session());
