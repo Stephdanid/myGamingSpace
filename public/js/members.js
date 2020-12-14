5b3a35ed424eb2828a742811a19d3d4d35d653bb
Ryan-Henson <72321799+Ryan-Henson@users.noreply.github.com>
2020-12-14 13:46:30 -0600

    updated a few buttons and api info

--- public/js/members.js
+++ public/js/members.js
@@ -1,11 +1,25 @@
+// console.log("hello world");
+
 $(document).ready(() => {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get('/api/user_data').then((data) => {
-        $('.member-name').text(data.email);
-    });
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then((data) => {
+    $(".member-name").text(data.email);
+  });
 });
 
 // / listening for submit to search for a game
 
+$("form").on("submit", (event) => {
+  event.preventDefault();
+  const userSearch = $("#searchedGame").val();
+  //   console.log(event);
+  window.location.replace("/api/games/" + userSearch);
+});
+
 // listening for delete button
+
+// $("#removeGame").on("click", (event) => {
+//     event.preventDefault();
+
+//   });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,32 +2,35 @@
 // const path = require('path');
 
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require('../config/middleware/isAuthenticated');
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-    app.get('/', (req, res) => {
+  app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
-        if (req.user) {
-            res.redirect('/members');
-        }
-        res.render('signup');
+    if (req.user) {
+      res.redirect("/members");
+    } else {
+      res.render("signup");
+    }
     // res.sendFile(path.join(__dirname, '../public/signup.html'));
-    });
+  });
 
-    app.get('/login', (req, res) => {
+  app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
-        if (req.user) {
-            res.redirect('/members');
-        }
-        // res.sendFile(path.join(__dirname, '../public/login.html'));
-        res.render('login');
-    });
+    if (req.user) {
+      res.redirect("/members");
+    }
+    // res.sendFile(path.join(__dirname, '../public/login.html'));
+    else {
+      res.render("login");
+    }
+  });
 
-    // Here we've add our isAuthenticated middleware to this route.
-    // If a user who is not logged in tries to access
-    // this route they will be redirected to the signup page
-    app.get('/members', isAuthenticated, (req, res) => {
-        res.render('members');
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access
+  // this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, (req, res) => {
+    res.render("members");
     // res.sendFile(path.join(__dirname, '../public/members.html'));
-    });
+  });
 };
