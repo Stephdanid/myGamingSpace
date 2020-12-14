f5300d448b87281721ddd88760c3884ddca61d51
Jose Jimenez <jrjimenez221@gmail.com>
2020-12-14 13:29:42 -0600

    Added models holding tables for wishlist

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 // Requiring necessary npm packages
 const express = require('express');
 const session = require('express-session');
-// const dotenv = require('dotenv').config();
+require('dotenv').config();
 // Requiring passport as we've configured it
 const passport = require('./config/passport');
-
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require('./models');
-
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({extended: true}));
@@ -25,15 +23,13 @@ app.use(passport.session());
 require('./routes/html-routes.js')(app);
 require('./routes/api-routes.js')(app);
 require('./routes/oAuthServer.js')(app);
-
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
+db.sequelize.sync({force: true}).then(() => {
     app.listen(PORT, () => {
         console.log(
             '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
             PORT,
             PORT,
-            console.log(process.env),
         );
     });
 });
